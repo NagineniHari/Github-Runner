6f73c03b9e27b165431b3c43923fc2f353d02e1d
naginenihari <naginenihari@gmail.com>
2026-01-28 12:23:48 +0530

    GithubRunner code is created

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+    default = "safety"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_name" {
+  type        = string
+  default     = "naginenihariaws.store"
+  description = "description"
+}
+
+variable "zone_id" {
+  type        = string
+  default     = "Z094097324SQZ3Z5XFAJS"
+  description = "description"
+}
